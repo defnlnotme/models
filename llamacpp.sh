a258318ee5dcaa5fd0970634d88da91645c41bbf
defnlnotme <a9pbg0vj6@mozmail.com>
2026-02-07 04:59:07 +0100

    feat: Add a completion benchmark script and update the default model in `llamacpp.sh`.

--- llamacpp.sh
+++ llamacpp.sh
@@ -6,7 +6,11 @@ MODELS_PATH="$HOME/data/models/gguf"
 #MODEL="/models/unsloth/Qwen3-4B-Instruct-2507-GGUF/Qwen3-4B-Instruct-2507-UD-Q4_K_XL.gguf"
 #MODEL="/models/unsloth/Magistral-Small-2509-GGUF-UD-Q4_K_XL/Magistral-Small-2509-UD-Q4_K_XL.gguf"
 #MODEL="/models/unsloth/Magistral-Small-2509-GGUF-UD-Q4_K_XL/Magistral-Small-2509-UD-Q4_K_XL.gguf"
-MODEL="/models/unsloth/Mistral-Small-3.2-24B-Instruct-2506-GGUF-UD-Q4_K_XL/Mistral-Small-3.2-24B-Instruct-2506-UD-Q4_K_XL.gguf"
+#MODEL="/models/unsloth/Mistral-Small-3.2-24B-Instruct-2506-GGUF-UD-Q4_K_XL/Mistral-Small-3.2-24B-Instruct-2506-UD-Q4_K_XL.gguf"
+#MODEL="/models/unsloth/Devstral-Small-2-24B-Instruct-2512-GGUF-UD-Q4_K_XL/Devstral-Small-2-24B-Instruct-2512-UD-Q4_K_XL.gguf"
+#MODEL="/models/unsloth/Magistral-Small-2509-GGUF-UD-Q4_K_XL/Magistral-Small-2509-UD-Q4_K_XL.gguf"
+MODEL="/models/unsloth/Ministral-3-8B-Instruct-2512-GGUF-UD-Q4_K_XL/Ministral-3-8B-Instruct-2512-UD-Q4_K_XL.gguf"
+
 DEVICES="--device /dev/dri/card0:/dev/dri/card0 --device /dev/dri/renderD128:/dev/dri/renderD128 --device /dev/dri/card1:/dev/dri/card1 --device /dev/dri/renderD129:/dev/dri/renderD129 --device /dev/accel:/dev/accel"
 IMAGE=llama-cpp-intel
 #IMAGE=llama-cpp-vulkan
