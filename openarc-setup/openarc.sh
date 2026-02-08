0466d8116defec2065db375daf32d81213f69b82
defnlnotme <a9pbg0vj6@mozmail.com>
2026-02-08 18:18:33 +0100

    fix

--- openarc-setup/openarc.sh
+++ openarc-setup/openarc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 MODEL_PATH="$HOME/data/models"
 DEVICES="--device /dev/accel:/dev/accel --device /dev/dri/card0:/dev/dri/card0 --device /dev/dri/renderD128:/dev/dri/renderD128 --device /dev/dri/card1:/dev/dri/card1 --device /dev/dri/renderD129:/dev/dri/renderD129"
 MODEL=""
@@ -12,7 +14,7 @@ docker run --security-opt label=disable \
 --env OPENARC_API_KEY=abc \
 --net=host \
 -v "$MODEL_PATH":"$MODEL_PATH" \
--v "$PWD/openarc-entrypoint.sh:/openarc-entrypoint.sh" \
+-v "$SCRIPT_DIR/openarc-entrypoint.sh:/openarc-entrypoint.sh" \
 --entrypoint=/openarc-entrypoint.sh \
 $DEVICES \
 $IMAGE \
